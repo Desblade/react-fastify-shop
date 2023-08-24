0ad650dc41a4cd1756a8510c1d7670c6dcc5d290
Наталья Галушкина <ngalushkina@heapp.game>
2023-08-24 21:57:39 +0300

    fix items price in cart

--- server/utils/convertPriceItemsWithTotalPrice.js
+++ server/utils/convertPriceItemsWithTotalPrice.js
@@ -1,5 +1,5 @@
 const convertPriceItemsWithTotalPrice = async (items, course) => {
-  const itemsWithTotalPrice = await Promise.all(items.map((item) => {
+  const itemsWithTotalPrice = await Promise.all(items.map(async (item) => {
     item.price = ((item.price / course).toFixed(2)) * item.count;
 
     return item;
